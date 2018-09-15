099ef6e0d40e7cf2361613149a935b7dd55aa9d5
cweichlein <43284421+cweichlein@users.noreply.github.com>
2018-09-14 18:11:41 -0700

    Made changes from GitHub.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main()
 {
-
+  cout<<"Helloo World!"<<endl;
+  cout<<"Input a number: ";
+  int num = 0;
+  cin >> num;
+  if(num > 0)
+  {
+    cout<<"This number is positive!"<<endl;
+  }
+  else
+  {
+    cout<<"This number is negative!"<<endl;
+  }
 
   cout << endl;
   system("Pause");
